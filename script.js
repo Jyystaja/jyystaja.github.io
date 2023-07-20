3370a6912147ec27dfe01bfda2c7df1668078b6c
Jyystaja <92653578+Jyystaja@users.noreply.github.com>
2023-07-20 12:22:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeHTML() {
+  // Get the 'output' div element
+  const outputDiv = document.getElementById('output');
+
+  // Change the HTML content of the 'output' div
+  outputDiv.innerHTML = '<p>HTML content changed!</p>';
+}
