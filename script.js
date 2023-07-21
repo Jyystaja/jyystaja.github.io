3537c8c8c4d38b87b1b6b195769c9be91f287c5d
Jyystaja <92653578+Jyystaja@users.noreply.github.com>
2023-07-21 13:43:48 +0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ function changeBigImage(imageSrc) {
   const bigImage = document.getElementById('bigImage');
   bigImage.src = imageSrc;
 }
+function toggleInfo() {
+  const infoSection = document.getElementById('infoSection');
+  infoSection.classList.toggle('open');
+}
+
 
