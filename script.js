475d13f3e9da7fe8ea747eb95134fb1ea7bf8e04
Jyystaja <92653578+Jyystaja@users.noreply.github.com>
2023-07-21 10:30:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function changeHTML() {
   window.location.href = 'tuote1.html';
 }
 
+function changeBigImage(imageSrc) {
+  const bigImage = document.getElementById('bigImage');
+  bigImage.src = imageSrc;
+}
+
